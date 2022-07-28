6060f4de98d94b0a5cdc07efeb19663ab3ca8a47
dzgoldman <dzgoldman@wesleyan.edu>
2022-07-28 10:47:44 -0400

    add set dont report script

---
+++ tasks/setDontReport.ts
@@ -0,0 +1,48 @@
+import { log, reportUnredeemed, wait } from "./lib";
+import yargs from "yargs/yargs";
+import { Retryable } from "../db/models";
+
+const { l1TxHash, msgIndex, chainid: ArbchainId, explanation } = yargs(
+  process.argv.slice(2)
+)
+  .options({
+    l1TxHash: {
+      type: "string",
+      demandOption: true,
+      description: "Target Tx Hash"
+    },
+    msgIndex: {
+      type: "number",
+      demandOption: true,
+      description: "Target Tx msg number"
+    },
+    chainid: {
+      type: "number",
+      demandOption: true,
+      description: "Target arb chainId"
+    },
+    explanation: {
+      type: "string",
+      demandOption: true,
+      description: ""
+    }
+  })
+  .parseSync();
+
+(async () => {
+  const msg = await Retryable.findOne({
+    where: {
+      l1TxHash,
+      msgIndex,
+      ArbchainId
+    }
+  });
+
+  if (!msg) throw new Error("Msg not found");
+  await msg.update({
+    dontReport: true
+  });
+  log(
+    `Will not report about msg with l1TxnHash ${l1TxHash} (index ${msgIndex}). Reason:'${explanation}'`
+  );
+})();
