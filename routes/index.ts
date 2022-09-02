6664f3f0645bb7aa960efbec43b281cce75b0877
dzgoldman <dzgoldman@wesleyan.edu>
2022-09-02 11:45:03 -0400

    add CORS

--- routes/index.ts
+++ routes/index.ts
@@ -2,12 +2,17 @@ import express, { Application, Request, Response } from "express";
 import db from "../db/config";
 import { Arbchain, Retryable } from "../db/models";
 import { L1ToL2MessageStatus } from "@arbitrum/sdk";
+import cors from 'cors'
+
 require("dotenv").config();
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.urlencoded({ extended: true }));
 try {
   db.authenticate();
