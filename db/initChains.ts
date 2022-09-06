5218b4712ca66194fccf9317e95b753a9f4f3736
Jason_Wanxt <wanxutao123@gmail.com>
2022-09-06 14:52:30 +0800

    move l2 rpc to env settings

--- db/initChains.ts
+++ db/initChains.ts
@@ -6,7 +6,7 @@ export const initChainsProgress = async () => {
   await ArbChain.create({
     id: 42161,
     lastBlockChecked: 15210167,
-    l2rpcURL: "https://Arb1-graph.arbitrum.io/rpc",
+    l2rpcURL: process.env.L2_ONE_RPC,
     l1rpcURL: "https://mainnet.infura.io/v3/" + process.env.INFURA_ID,
     inboxAddress: "0x4Dbd4fc535Ac27206064B68FfCf827b0A60BAB3f"
   })
@@ -15,7 +15,7 @@ export const initChainsProgress = async () => {
   await ArbChain.create({
     id: 42170,
     lastBlockChecked: 15219858,
-    l2rpcURL: "https://a4ba.arbitrum.io/rpc",
+    l2rpcURL: process.env.L2_NOVA_RPC,
     l1rpcURL: "https://mainnet.infura.io/v3/" + process.env.INFURA_ID,
     inboxAddress: "0xc4448b71118c9071bcb9734a0eac55d18a153949"
   })
--- src/main.ts
+++ src/main.ts
@@ -13,21 +13,21 @@ const main = async () => {
     switch (argv.action) {
         case "sync":
             if(!argv.chainid) {
-                console.error("Error: chainid needed");
+                console.error("Error: arg chainid needed");
                 exit(1);
             }
             return syncRetryablesProcess();
 
         case "update":
             if(!argv.chainid) {
-                console.error("Error: chainid needed");
+                console.error("Error: arg chainid needed");
                 exit(1);
             }
             return updateProcess();
 
         case "report":
             if(!argv.chainids) {
-                console.error("Error: chainids needed");
+                console.error("Error: arg chainids needed");
                 exit(1);
             }
             return reportUnredeemedProcess();
@@ -36,15 +36,20 @@ const main = async () => {
             return appProgress();
 
         case "init_db":
+            if(!process.env.L2_ONE_RPC || !process.env.L2_NOVA_RPC) {
+                if(!process.env.L2_ONE_RPC) console.error("Error: env L2_ONE_RPC needed");
+                if(!process.env.L2_NOVA_RPC) console.error("Error: env L2_NOVA_RPC needed");
+                exit(1);
+            }
             await resetDBProgress();
             return initChainsProgress();
 
         case "set_dont_report":
             if(!argv.l1TxHash || !argv.msgIndex || !argv.chainid || !argv.explanation) {
-                if(!argv.l1TxHash) console.error("Error: l1TxHash needed");
-                if(!argv.msgIndex) console.error("Error: msgIndex needed");
-                if(!argv.chainid) console.error("Error: chainid needed");
-                if(!argv.explanation) console.error("Error: explanation needed");
+                if(!argv.l1TxHash) console.error("Error: arg l1TxHash needed");
+                if(!argv.msgIndex) console.error("Error: arg msgIndex needed");
+                if(!argv.chainid) console.error("Error: arg chainid needed");
+                if(!argv.explanation) console.error("Error: arg explanation needed");
                 exit(1);
             }
             
